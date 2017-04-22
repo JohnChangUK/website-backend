bf6d8d09e4c476c1d73c93e602a252632f7474c8
John Chang <johnchang9094@gmail.com>
2017-04-22 20:39:35 +0100

    Added Promise to controllers function, added Profile Controller

---
+++ controllers/ProfileController.js
@@ -0,0 +1,50 @@
+var Profile = require('../models/Profile');
+var Promise = require('bluebird');
+
+module.exports = {
+
+    get: function(params) {
+        
+        return new Promise(function(resolve, reject) {
+            Profile.find(params, function(err, profiles) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve(profiles);
+            });
+        });
+    },
+
+    getById: function(id) {
+        return new Promise(function(resolve, reject) {
+            Profile.findById(id, function(err, profile) {
+                if (err) {
+                    reject(new Error('Profile not Found!'));
+                    return;
+                }
+
+                if (profile == null) {
+                    reject(new Error('Profile not found'));
+                    return;
+                }
+
+                resolve(profile);
+            });
+        });
+    },
+
+    post: function(body) {
+        return new Promise(function(resolve, reject) {
+            Profile.create(body, function(err, profile) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve(profile);
+            });
+        });
+    }
+};
