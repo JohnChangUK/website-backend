348ec7ecf3278f1a582be62a8372a5f0b9493d8e
John Chang <johnchang9094@gmail.com>
2017-04-22 22:50:32 +0100

    summary() to only show email, username and NOT password

--- models/Profile.js
+++ models/Profile.js
@@ -8,4 +8,16 @@ var ProfileSchema = new mongoose.Schema({
 
 });
 
+ProfileSchema.methods.summary = function() {
+    
+    var summary = {
+        email: this.email,
+        username: this.username,
+        timestamp: this.timestamp,
+        id: this._id.toString()
+    };
+
+    return summary;
+};
+
 module.exports = mongoose.model('ProfileSchema', ProfileSchema);
